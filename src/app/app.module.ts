f7b1afe03d6aacdbf103df5d7ad2e0205c3f2ea4
Valdomiro Santos <valdomiro.jose@outlook.com>
2024-08-19 01:10:11 -0300

    projeto finalizado - totalmente reutilizavel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BalanceCardComponent } from './components/balance-card/balance-card.component';
+import { CreditCardComponent } from './components/credit-card/credit-card.component';
+import { DebitCardComponent } from './components/debit-card/debit-card.component';
+import { ClientInfosCardComponent } from './components/client-infos-card/client-infos-card.component';
+import { CardItemComponent } from './components/card-item/card-item.component';
+import { CardLineComponent } from './components/card-line/card-line.component';
+import { CardTitleComponent } from './components/card-title/card-title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BalanceCardComponent,
+    CreditCardComponent,
+    DebitCardComponent,
+    ClientInfosCardComponent,
+    CardItemComponent,
+    CardLineComponent,
+    CardTitleComponent
   ],
   imports: [
     BrowserModule,
